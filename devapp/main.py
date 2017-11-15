0ad87c41ec76eb9068e005ce4c007646d68f6e51
Nelson Omuto <nelsonomuto@gmail.com>
2017-11-15 11:00:46 -0500

    update webpack

--- devapp/main.py
+++ devapp/main.py
@@ -1,26 +1,41 @@
 import os, errno, sys
 from os import path
 from distutils.dir_util import copy_tree
+from lib.files import replace_in_file
 
+default_port = '3006'
 dev_app_dir = path.abspath(sys.argv[1])
 component_dir = path.abspath(sys.argv[2])
+port = sys.argv[3] if(len(sys.argv) >= 4) else default_port
 component_name = str.split(component_dir, '/')[-1]
+dev_app_component_dir = path.join(dev_app_dir, 'src', component_name)
 file_parent = path.dirname(os.path.realpath(__file__))
 template_dir = path.join(file_parent, 'template')
-dev_app_component_dir = path.join(dev_app_dir, 'src', component_name)
 
 print(f"""
     dev_app_dir {dev_app_dir}
+    port {port}
     component_dir {component_dir}
     component_name {component_name}
+    dev_app_component_dir {dev_app_component_dir}    
     file_parent {file_parent}
     template_dir {template_dir}
 """)
 
+def get_template_list():
+    template_list = [
+        ('{port}', port)
+    ]
+
 def init_template(app_dir):
     assert not path.exists(app_dir)
     os.makedirs(app_dir)
+    os.makedirs(path.join(dev_app_dir, 'src'))    
     copy_tree(template_dir, app_dir)
+
+    for root, dirs, files in os.walk(app_dir):
+        for file in files:
+            replace_in_file(file, get_template_list())
 
 def init():
     init_template(dev_app_dir)
--- devapp/template/webpack.config.js
+++ devapp/template/webpack.config.js
@@ -1,21 +1,12 @@
-const _ = require('lodash');
 const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const {
-    config,
-    production,
-    getLessLoaderConfig,
-    CopyPlugin,
-    ImagePathResolverPlugin,
-    PolyfillPlugin
-} = require('./webpack.config.template.js')('./', webpack, _, path, CleanWebpackPlugin);
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 
-config.entry = {
-    'hotel-details': path.resolve(__dirname, './app/hotel-details.js')
-};
 const sassThreadLoader = require('thread-loader');
 
 sassThreadLoader.warmup({ workerParallelJobs: 2 }, [
@@ -25,82 +16,304 @@ sassThreadLoader.warmup({ workerParallelJobs: 2 }, [
     'style-loader',
     'babel-loader'
 ]);
-const extractPlugins = [];
-const extractPluginsRules = [];
 
-config.output.publicPath = '/hotel/assets/ui-hotel-details';
-config.plugins = config.plugins.concat([
-    new CleanWebpackPlugin(['public'], {
-        root: path.resolve(__dirname),
-        verbose: true,
-        dry: false
-    }),
-    /* eslint-disable */
-    ... !production ? [] : [new UglifyJSPlugin({ // only uglify in production, uglify is slow, no need for this in development even though it happens in parallel which ameliorates the pain
-        parallel: true
-    })],
+// replace localhost with 0.0.0.0 if you want to access
+// your app from wifi or a virtual machine
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || {port};
+const sourcePath = path.join(__dirname, './src');
+const buildDirectory = path.join(__dirname, './build');
 
-    new ImagePathResolverPlugin(),
+const stats = {
+    assets: true,
+    children: false,
+    chunks: false,
+    hash: false,
+    modules: false,
+    publicPath: false,
+    timings: true,
+    version: false,
+    warnings: true,
+    colors: {
+        green: '\u001b[32m'
+    }
+};
 
-    ... production ? extractPlugins : [], // use inline css in development for debugging & speed
+module.exports = function(env) {
+    const nodeEnv = env && env.prod ? 'production' : 'development';
+    const isProd = nodeEnv === 'production';
 
-    new CopyPlugin([
-        {
-            from: path.resolve(__dirname, 'images'),
-            to: [
-                path.resolve(__dirname, 'public/images')
+    const serviceWorkerBuild = env && env.sw;
+
+    const htmlTemplate = isProd ? 'index.prod.ejs' : 'index.dev.ejs';
+
+    let cssLoader;
+
+    const plugins = [
+        new webpack.optimize.CommonsChunkPlugin({
+            async: true,
+            children: true,
+            minChunks: 2
+        }),
+
+        // setting production environment will strip out
+        // some of the development code from the app
+        // and libraries
+        new webpack.DefinePlugin({
+            'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+        }),
+
+        // create index.html
+        new HtmlWebpackPlugin({
+            template: htmlTemplate,
+            inject: true,
+            production: isProd,
+            preload: ['*.css'],
+            minify: isProd && {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        }),
+
+        // make sure script tags are async to avoid blocking html render
+        new ScriptExtHtmlWebpackPlugin({
+            defaultAttribute: 'async',
+            sync: /^libs.dll.*$/,
+            preload: {
+                test: /^0|^main|^style-.*$/,
+                chunks: 'all'
+            }
+        })
+    ];
+
+    if (isProd) {
+        plugins.push(
+            // create css bundle
+            new ExtractTextPlugin('style-[contenthash:8].css'),
+            // minify remove some of the dead code
+            new UglifyJSPlugin({
+                compress: {
+                    warnings: false,
+                    screw_ie8: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true
+                }
+            })
+        );
+
+        cssLoader = ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'cache-loader',
+                {
+                    loader: 'thread-loader',
+                    options: {
+                        workerParallelJobs: 2
+                    }
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        module: true, // css-loader 0.14.5 compatible
+                        modules: true,
+                        importLoaders: 1,
+                        localIdentName: '[hash:base64:5]'
+                    }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        outputStyle: 'collapsed',
+                        sourceMap: true,
+                        includePaths: [sourcePath]
+                    }
+                }
             ]
-        }
-    ]),
-    /* eslint-enable */
-    new CopyPlugin([
-        {
-            from: path.resolve(
-                __dirname,
-                'node_modules/ui-legacy-common/style-guide/app/lib/icons/dist/fonts'
-            ),
-            to: [path.resolve(__dirname, 'public/lib/icons/dist/fonts')]
-        },
-        {
-            // TODO: Deprecate, will be part of core styles
-            from: path.resolve(
-                __dirname,
-                'node_modules/ui-legacy-common/web/public/stylesheets/lib'
-            ),
-            to: [path.resolve(__dirname, 'public/stylesheets/lib')]
-        }
-    ])
-]);
+        });
+    } else {
+        plugins.push(
+            // make hot reloading work
+            new webpack.HotModuleReplacementPlugin(),
+            // show module names instead of numbers in webpack stats
+            new webpack.NamedModulesPlugin(),
+            // don't spit out any errors in compiled assets
+            new webpack.NoEmitOnErrorsPlugin(),
+            // load DLL files
+            /* eslint-disable global-require */
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./dll/libs-manifest.json')
+            }),
+            /* eslint-enable global-require */
 
-config.module.rules = config.module.rules.concat([
-    {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        include: [path.resolve(__dirname, './app')],
-        use: [
+            // make DLL assets available for the app to download
+            new AddAssetHtmlPlugin([{ filepath: require.resolve('./dll/libs.dll.js') }])
+        );
+
+        cssLoader = [
+            // cache css output for faster rebuilds
+            'cache-loader',
             {
+                // build css/sass in threads (faster)
                 loader: 'thread-loader',
                 options: {
                     workerParallelJobs: 2
                 }
             },
             {
-                loader: 'babel-loader',
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader',
                 options: {
-                    presets: ['react', 'env']
+                    module: true,
+                    importLoaders: 1,
+                    localIdentName: '[path][name]-[local]'
+                }
+            },
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: false,
+                    includePaths: [sourcePath]
                 }
             }
-        ]
-    },
-    {
-        test: /\.txt$|\.handlebars$/,
-        use: 'raw-loader'
-    },
-    ...(production ? extractPluginsRules : []) // only used in production mode
-]);
+        ];
+    }
 
-config.resolve.alias = _.extend(config.resolve.alias, {
-    app: path.resolve(__dirname, 'app')
-});
+    if (serviceWorkerBuild) {
+        plugins.push(
+            new SWPrecacheWebpackPlugin({
+                cacheId: 'budgeting-app',
+                filename: 'sw.js',
+                maximumFileSizeToCacheInBytes: 800000,
+                mergeStaticsConfig: true,
+                minify: true,
+                runtimeCaching: [
+                    {
+                        handler: 'cacheFirst',
+                        urlPattern: /(.*?)/
+                    }
+                ]
+            })
+        );
+    }
 
-module.exports = config;
+    const entryPoint = isProd
+        ? './index.js'
+        : [
+              // activate HMR for React
+              'react-hot-loader/patch',
+
+              // bundle the client for webpack-dev-server
+              // and connect to the provided endpoint
+              `webpack-dev-server/client?http://${host}:${port}`,
+
+              // bundle the client for hot reloading
+              // only- means to only hot reload for successful updates
+              'webpack/hot/only-dev-server',
+
+              // the entry point of our app
+              './index.js'
+          ];
+
+    return {
+        devtool: isProd ? 'cheap-source-map' : 'eval-cheap-module-source-map',
+        context: sourcePath,
+        entry: {
+            main: entryPoint
+        },
+        output: {
+            path: buildDirectory,
+            publicPath: '/',
+            // Computing hashes is expensive and we don't need them in development
+            filename: isProd ? '[name]-[hash:8].js' : '[name].js',
+            chunkFilename: isProd ? '[name]-[chunkhash:8].js' : '[name].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(html|svg|jpe?g|png|ttf|woff2?)$/,
+                    include: sourcePath,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            name: isProd ? 'static/[name]-[hash:8].[ext]' : 'static/[name].[ext]'
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    include: sourcePath,
+                    use: cssLoader
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    include: sourcePath,
+                    use: [
+                        {
+                            loader: 'thread-loader',
+                            options: {
+                                workerParallelJobs: 2
+                            }
+                        },
+                        'babel-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.scss'],
+            modules: [path.resolve(__dirname, 'node_modules'), sourcePath],
+            symlinks: false
+        },
+
+        plugins,
+
+        performance: isProd && {
+            maxAssetSize: 300000,
+            maxEntrypointSize: 300000,
+            hints: 'warning'
+        },
+
+        stats: stats,
+
+        devServer: {
+            contentBase: './app',
+            publicPath: '/',
+            historyApiFallback: true,
+            port: port,
+            host: host,
+            hot: !isProd,
+            compress: isProd,
+            stats: stats
+        }
+    };
+};
